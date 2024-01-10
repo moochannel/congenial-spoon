dffab38ad0efb6e6462b46d59a0ccd1f44d60613
Katsuhisa Ueda <moochannel@gmail.com>
2024-01-10 10:12:22 +0900

    Store access token in session

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="@auth/sveltekit" />
+
+import "@auth/sveltekit"
+
+declare module "@auth/sveltekit" {
+  interface Session {
+    accessToken: string
+  }
+}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,4 +4,16 @@ import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+	callbacks: {
+		jwt: async ({ token, account }) => {
+			if (account) {
+				token.accessToken = account.access_token
+			}
+			return token
+		},
+		session: async ({ session, token }) => {
+			session.accessToken = token.accessToken as string
+			return session
+		},
+	},
 })
